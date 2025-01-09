06ccc82cb89769ddd64a078a3d46294c3f6b71d5
MohanrajRK <159987765+MohanrajRK@users.noreply.github.com>
2025-01-09 15:26:34 +0530

    932646: Enhancing the UI for a More Attractive Stock Management Application with tailwind3.

--- Stock-Market-Application/src/components/Overview.tsx
+++ Stock-Market-Application/src/components/Overview.tsx
@@ -175,6 +175,17 @@ export default function Overview(props: { changeMarquee: Function, myStockDm: Da
       let query: Query = new Query().where(Predicate.or(predicates));
       (props.myStockDm as any).persistQuery = query;
       props.myStockDm.setPersistData({} as any, 'myStocks', query);
+      if (myWishList.length) {
+        const myWishlistIcon = args.target.querySelector('.addmywishlist');
+        if (myWishlistIcon) {
+          myWishlistIcon.classList.remove('addmywishlist');
+          myWishlistIcon.classList.add('addedmywishlist');
+        }
+        if (args.target.classList.contains('e-primary')) {
+          args.target.classList.remove('e-primary');
+          args.target.classList.add('e-success');
+        }
+      }
     }
     if (args.target!.querySelector('.analysis')) {
       navigate('/stock_analysis', {
@@ -233,6 +244,7 @@ export default function Overview(props: { changeMarquee: Function, myStockDm: Da
               enableHover={false}
               commandClick={commandClick}
               destroyed={destroyed}
+              height={341}
             >
               <ColumnsDirective>
                 <ColumnDirective
@@ -308,14 +320,14 @@ export default function Overview(props: { changeMarquee: Function, myStockDm: Da
                       title: 'Add to Wishlist',
                       buttonOption: {
                         iconCss: 'addmywishlist e-icons',
-                        cssClass: 'e-flat',
+                        cssClass: 'e-primary' ,
                       },
                     },
                     {
                       title: 'Analysis',
                       buttonOption: {
                         iconCss: 'analysis e-icons',
-                        cssClass: 'e-flat',
+                        cssClass: 'e-primary',
                       },
                     },
                   ]}
